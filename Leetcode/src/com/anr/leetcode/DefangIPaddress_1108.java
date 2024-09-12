e128db2ec7d9c17b3622768ef4b478fa5671d67e
Ajay <ajayramavath12345@gmail.com>
2024-09-12 17:47:38 +0530

    leetCode 1108

--- Leetcode/src/com/anr/leetcode/DefangIPaddress_1108.java
+++ Leetcode/src/com/anr/leetcode/DefangIPaddress_1108.java
@@ -4,10 +4,8 @@ public class DefangIPaddress_1108 {
 
 	public static void main(String[] args) {
 		defangIPaddress("123.21.14.123");
-		System.out.println(defangIPaddress("123.21.14.123"));
-
+		defangIPaddress("123.21.14.123");
 	}
-
 	private static String defangIPaddress(String address) {
 		String ans = "";
 		for (int i = 0; i < address.length(); i++) {
@@ -19,7 +17,5 @@ public class DefangIPaddress_1108 {
 			}
 		}
 		return ans;
-
 	}
-
 }
